1baa51cc80ec3308feb422688290270413924d0c
Jhoneider <31227274+Jhoneider@users.noreply.github.com>
2024-11-27 00:51:02 -0500

    cambio de titulo

--- index (4).php
+++ index (4).php
@@ -40,7 +40,7 @@ $result_monthly_avg = $conn->query($sql_monthly_avg);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Datos Estadísticos</title>
+    <title>mis datos estadisticos </title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
